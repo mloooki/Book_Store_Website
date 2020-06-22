7d2bcd7e0b89a72cefd73eba1911e8ec420192a0
MLoooKi <mloooki.3@gmail.com>
2020-06-22 18:02:09 +0300

    feat: Add slide show to the main page

--- Main.php
+++ Main.php
@@ -82,7 +82,6 @@
                                      setTimeout(carousel, 2000); // Change image every 2 seconds
                                     }
          </script> 
-
                
          <footer>
 			    <p>
